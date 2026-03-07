f8a342e554f4c617f21abecb9566cb512672e77d
@subashkarki321 <subashkarki321@gmail.com>
2026-03-07 22:27:44 +0545

    merge with the main branch

--- lib/api/axiosConfig.ts
+++ lib/api/axiosConfig.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "https://api.shristiuniverse.com";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://api.shristiuniverse.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
